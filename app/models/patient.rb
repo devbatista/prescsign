7c62af2f96a1333b0a61557b88704402715538a1
Rafael Batista <rafael@devbatista.com>
2026-04-06 21:36:39 -0300

    criando tabela de pacientes

---
+++ app/models/patient.rb
@@ -0,0 +1,11 @@
+class Patient < ApplicationRecord
+  validates :full_name, presence: true, length: { minimum: 3 }
+  validates :cpf, presence: true, uniqueness: true, length: { minimum: 11 }
+  validates :birth_date, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, allow_blank: true
+  validates :phone, length: { minimum: 10 }, allow_blank: true
+
+  normalizes :cpf, with: ->(value) { value&.gsub(/\D/, "") }
+  normalizes :email, with: ->(value) { value&.strip&.downcase }
+  normalizes :phone, with: ->(value) { value&.gsub(/\D/, "") }
+end
