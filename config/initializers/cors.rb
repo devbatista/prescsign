c63027dbe6111c7f88413150285356c3802593ab
Rafael Batista <rafael@devbatista.com>
2026-04-17 11:48:30 -0300

    configurando CORS restrito

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,13 @@
-# Be sure to restart your server when you modify this file.
+allowed_origins = Rails.application.config.x.cors.allowed_origins
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins(*allowed_origins)
 
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource "*",
+             headers: %w[Accept Authorization Content-Type Origin X-Organization-Id],
+             expose: %w[X-Request-Id],
+             methods: %i[get post put patch delete options head],
+             max_age: 600
+  end
+end
